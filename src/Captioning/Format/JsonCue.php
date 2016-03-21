fc496e791244446cfc33b486276fa2d7d4f776ca
delphiki <gdelphiki@gmail.com>
2016-03-21 20:38:34 +0100

    Add basic JSON (TED) subtitles support

---
+++ src/Captioning/Format/JsonCue.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Captioning\Format;
+
+use Captioning\Cue;
+
+class JsonCue extends Cue
+{
+    /**
+     * @var bool
+     */
+    protected $startOfParagraph;
+
+    /**
+     * @var integer
+     */
+    protected $duration;
+
+    /**
+     * @param string $_timecode
+     * @return null
+     */
+    public static function tc2ms($_timecode)
+    {
+        return $_timecode;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isStartOfParagraph()
+    {
+        return $this->startOfParagraph;
+    }
+
+    /**
+     * @param $startOfParagraph
+     * @return $this
+     */
+    public function setStartOfParagraph($startOfParagraph)
+    {
+        $this->startOfParagraph = $startOfParagraph;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param $duration
+     * @return $this
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $_start
+     * @return $this
+     */
+    public function setStart($_start)
+    {
+        parent::setStart($_start);
+
+        if ($this->stop) {
+            $this->duration = $this->stop - $this->start;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $_stop
+     * @return $this
+     */
+    public function setStop($_stop)
+    {
+        parent::setStop($_stop);
+
+        if ($this->start) {
+            $this->duration = $this->stop - $this->start;
+        }
+
+        return $this;
+    }
+}
---
+++ src/Captioning/Format/JsonFile.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delphiki
+ * Date: 18/03/16
+ * Time: 19:46
+ */
+
+namespace Captioning\Format;
+
+use Captioning\File;
+
+class JsonFile extends File
+{
+    public function parse()
+    {
+        $decodedContent = json_decode($this->fileContent, true);
+
+        if (!isset($decodedContent['captions'])) {
+            throw new \InvalidArgumentException('Invalid JSON subtitle');
+        }
+
+        if (count($decodedContent['captions']) == 0) {
+            return false;
+        }
+
+        foreach ($decodedContent['captions'] as $c) {
+            $cue = new JsonCue($c['startTime'], $c['startTime'] + $c['duration'], $c['content']);
+            $cue->setDuration($c['duration']);
+            $cue->setStartOfParagraph($c['startOfParagraph']);
+
+            $this->addCue($cue);
+        }
+
+        return $this;
+    }
+
+    public function buildPart($_from, $_to)
+    {
+        $this->sortCues();
+
+        if ($_from < 0 || $_from >= $this->getCuesCount()) {
+            $_from = 0;
+        }
+
+        if ($_to < 0 || $_to >= $this->getCuesCount()) {
+            $_to = $this->getCuesCount() - 1;
+        }
+
+
+        $captions = array();
+        for ($j = $_from; $j <= $_to; $j++) {
+            /** @var JsonCue $cue */
+            $cue = $this->getCue($j);
+
+            $captions[] = array(
+                'duration'         => $cue->getDuration(),
+                'content'          => $cue->getText(),
+                'startOfParagraph' => $cue->isStartOfParagraph(),
+                'startTime'        => $cue->getStart(),
+            );
+        }
+
+        $this->fileContent = json_encode(array('captions' => $captions));
+
+        return $this;
+    }
+
+}
